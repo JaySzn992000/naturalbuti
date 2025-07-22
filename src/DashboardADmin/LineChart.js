b350898ab9bd67d444e4040996f37fc023361667
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-07-22 07:39:55 +0530

    Update LineChart.js

--- src/DashboardADmin/LineChart.js
+++ src/DashboardADmin/LineChart.js
@@ -36,7 +36,7 @@ data: [],
 useEffect(() => {
 const customerFetch = async () => {
 try {
-const response = await fetch("https://nitiaryapickle.onrender.com/fetchCutomerOrder");
+const response = await fetch("https://naturalbuti.onrender.com/fetchCutomerOrder");
 const data = await response.json();
 
 // Month-wise 
