abc3953dec232af69f95800c77078cf42db5834f
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-07-29 00:09:49 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -136,7 +136,8 @@ className="seeLLProducts">View All</button>
 <Contactus></Contactus>
 
 <a
-href='https://api.whatsapp.com/send/?phone=%2B919821187225&text&type=phone_number&app_absent=0'>
+href='https://api.whatsapp.com/send/?phone=%2B919821187225&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer"
+>
 <img
 className="whatsapp_message"
 src="https://cdn-icons-png.freepik.com/256/3983/3983877.png?semt=ais_hybrid"
