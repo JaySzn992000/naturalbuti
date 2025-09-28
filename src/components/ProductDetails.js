a900abac887e0e330069a71d20bb0b15a1090c49
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-09-28 21:04:30 +0530

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -28,7 +28,7 @@ const [message, setMessage] = useState("");
 
 
 useEffect(() => {
-fetch(`http://localhost:3001/products/${id}`)
+fetch(`https://naturalbuti.onrender.com/products/${id}`)
 .then(res => res.json())
 .then(data => {
 });
@@ -39,7 +39,7 @@ useEffect(() => {
 const fetchProduct = async () => {
 try {
 const response = await axios.get(
-"http://localhost:3001/fetchProductslist"
+"https://naturalbuti.onrender.com/fetchProductslist"
 );
 const data = response.data;
 const product = data.find((product) => product.id === parseInt(id));
@@ -168,7 +168,7 @@ content="https://yourdomain.com/images/pickle-default.jpg"
 
 <img
 className="product_img"
-src={`http://localhost:3001${arrayStore.file_path}`}
+src={`https://naturalbuti.onrender.com${arrayStore.file_path}`}
 alt=''
 loading="lazy"
 />
@@ -177,7 +177,7 @@ loading="lazy"
 
 <img
 className="product_img"
-src={`http://localhost:3001${arrayStore.file_path1}`}
+src={`https://naturalbuti.onrender.com${arrayStore.file_path1}`}
 alt=''
 loading="lazy"
 />
@@ -186,7 +186,7 @@ loading="lazy"
 
 <img
 className="product_img"
-src={`http://localhost:3001${arrayStore.file_path2}`}
+src={`https://naturalbuti.onrender.com${arrayStore.file_path2}`}
 alt=''
 loading="lazy"
 />
@@ -195,7 +195,7 @@ loading="lazy"
 
 <img
 className="product_img"
-src={`http://localhost:3001${arrayStore.file_path3}`}
+src={`https://naturalbuti.onrender.com${arrayStore.file_path3}`}
 alt=''
 loading="lazy"
 />
@@ -211,7 +211,7 @@ desktop view */}
 <div className="thumbnails-container">
 <img
 className="thumbnail"
-src={`http://localhost:3001${arrayStore.file_path1}`}
+src={arrayStore.file_path1}
 alt="home made pickles"
 loading="lazy"
 onClick={() => handleThumbnailClick(arrayStore.file_path1)}
@@ -219,7 +219,7 @@ onClick={() => handleThumbnailClick(arrayStore.file_path1)}
 
 <img
 className="thumbnail"
-src={`http://localhost:3001${arrayStore.file_path2}`}
+src={arrayStore.file_path2}
 alt="ingredients used pickles"
 loading="lazy"
 onClick={() => handleThumbnailClick(arrayStore.file_path2)}
@@ -227,7 +227,7 @@ onClick={() => handleThumbnailClick(arrayStore.file_path2)}
 
 <img
 className="thumbnail"
-src={`http://localhost:3001${arrayStore.file_path3}`}
+src={arrayStore.file_path3}
 alt="customized pickles"
 onClick={() => handleThumbnailClick(arrayStore.file_path3)}
 loading="lazy"
@@ -243,7 +243,7 @@ with zoom */}
 <Zoom>
 <img
 className="product_img"
-src={`http://localhost:3001${mainImage}`}
+src={mainImage}
 alt={arrayStore.name}
 loading="lazy"
 />
@@ -269,10 +269,9 @@ INR
 <h2 className="stckTg"> In Stock 
 { } <span>{arrayStore.stock}</span> 
 </h2>
-<p>Select Size</p>
+<p>Size</p>
 
 </section>
-
 
 <div className="size_chart">
 {sizes.map((size, index) => (
@@ -367,4 +366,3 @@ cart: state.cart,
 });
 
 export default connect(mapStateToProps, { addToCart })(ProductDetails);
-
